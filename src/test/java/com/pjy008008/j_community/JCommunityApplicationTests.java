67874d1944c7907fedfd2b4218d0addfa2d97917
Junyou Park <pjy008008@gmail.com>
2025-11-06 19:05:34 +0900

    init

---
+++ src/test/java/com/pjy008008/j_community/JCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjy008008.j_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
