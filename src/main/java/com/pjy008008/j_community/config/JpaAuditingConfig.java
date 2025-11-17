2be76d0fe0036931d4353c1df95258ec5ef9f761
Junyou Park <pjy008008@gmail.com>
2025-11-17 23:15:41 +0900

    feat(config): JPA Auditing 및 BaseTimeEntity 추가
    모든 엔티티가 생성/수정 시간을 자동으로 갖도록 @EnableJpaAuditing 설정을 추가하고,
    @MappedSuperclass인 BaseTimeEntity를 생성

---
+++ src/main/java/com/pjy008008/j_community/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.pjy008008.j_community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
---
+++ src/main/java/com/pjy008008/j_community/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.pjy008008.j_community.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
