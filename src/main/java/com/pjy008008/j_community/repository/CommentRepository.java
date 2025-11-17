562c0d1c766c6d7109e415e1f367e241817e2a53
Junyou Park <pjy008008@gmail.com>
2025-11-18 04:27:24 +0900

    feat(repository): CommentRepository에 계층형 조회 메서드 추가
    댓글/대댓글 기능 구현을 위해 CommentRepository를 수정
    - `findByPostId` (게시글의 모든 댓글 조회) 메서드 추가
    - `findByPostIdAndParentIsNullOrderByCreatedAtAsc` (최상위 댓글만 조회) 메서드 추가

--- src/main/java/com/pjy008008/j_community/repository/CommentRepository.java
+++ src/main/java/com/pjy008008/j_community/repository/CommentRepository.java
@@ -1,6 +1,11 @@
 package com.pjy008008.j_community.repository;
+
 import com.pjy008008.j_community.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+    List<Comment> findByPostIdAndParentIsNullOrderByCreatedAtAsc(Long postId);
 }
